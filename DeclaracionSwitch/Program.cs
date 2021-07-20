469f1f76e05e977a4aedea58b8fc08d9cfdbe9a8
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-19 20:16:48 -0500

    Básico

--- DeclaracionSwitch/Program.cs
+++ DeclaracionSwitch/Program.cs
@@ -6,7 +6,27 @@ namespace DeclaracionSwitch
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int autos = 1;
+
+            switch (autos)
+            {
+                case 0:
+                    Console.WriteLine("Usted no paga impuestos");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Usted paga 15$");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Usted paga 30$");
+                    break;
+
+                default:
+                    Console.WriteLine("Usted paga 15$ dolares por auto");
+                    break;
+            }
+            Console.Read();
         }
     }
 }
